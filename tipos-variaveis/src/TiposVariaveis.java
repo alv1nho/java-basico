e8219c7cd2e8408986c956d06734ebe3dceff15a
alv1nho <alvaro-dev@hotmail.com>
2023-09-11 21:28:19 -0300

    Tipos de variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Tipos primitivos;
+        double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // Váriaveis e Constantes;
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        // Constante: definindo "final" e o nome da váriavel em caixa alta;
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
+        // Strings;
+        String nome = "Alvaro Andrade";
+        System.out.println(nome);
     }
 }
